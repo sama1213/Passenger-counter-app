3871eac476b269b78212d8b23baf1905971611ad
Samaresh <samareshmondal53547@gmail.com>
2023-03-14 10:15:07 +0530

    This is a passebger counting app

---
+++ index.js
@@ -0,0 +1,24 @@
+
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+
+// console.log(saveEl);
+// console.log(countEl);
+
+let count = 0;
+
+function increment(){
+    count += 1;
+    countEl.textContent = count;
+    
+}
+
+function save(){
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+
+
+    // console.log(count);
+}
